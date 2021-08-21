c1437e986e3a221c7cf4de16799c9800ef99340f
Pallavi <pallavibobde1@gmail.com>
2021-08-21 06:56:05 +0530

    improved ui

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ let windspeed = document.querySelector(".windspeed");
 let humidity = document.querySelector(".humidity");
 let pressure = document.querySelector(".pressure");
 let bgimg = document.querySelector(".bgimg")
+let morebtn = document.querySelector(".more");
+let details = document.querySelector(".details");
 
 const userAction = (city) =>{
 
@@ -23,6 +25,7 @@ const userAction = (city) =>{
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
   .then(response => response.json())
   .then(jsondata => {
+      
       cityname.innerHTML = jsondata.name;
       countryname.innerHTML = jsondata.sys.country;
       temp.innerHTML =  (jsondata.main.temp -273.15).toFixed(1);
@@ -32,10 +35,30 @@ const userAction = (city) =>{
       windspeed.innerHTML = jsondata.wind.speed;
       humidity.innerHTML = jsondata.main.humidity;
       pressure.innerHTML = jsondata.main.pressure;
+      if(jsondata.weather[0].description.includes("cloud")||jsondata.weather[0].description.includes("rain")){
+        document.querySelector(".fa-cloud").style.display="block";
+        document.querySelector(".fa-bolt").style.display="none";
+        document.querySelector(".fa-sun").style.display="none";
+
+      }
+      else if(jsondata.weather[0].description.includes("thunderstorm")){
+        document.querySelector(".fa-cloud").style.display="none";
+        document.querySelector(".fa-bolt").style.display="block";
+        document.querySelector(".fa-sun").style.display="none";
+
+      }
+      else if(jsondata.weather[0].description.includes("sun")){
+        document.querySelector(".fa-cloud").style.display="none";
+        document.querySelector(".fa-bolt").style.display="none";
+        document.querySelector(".fa-sun").style.display="block";
+
+      }
+      else{
+        document.querySelector(".fa-cloud").style.display="none";
+        document.querySelector(".fa-bolt").style.display="none";
+        document.querySelector(".fa-sun").style.display="none";
+      }
       bgimg.src = `https://source.unsplash.com/1600x900/?${jsondata.weather[0].description}`;
-      console.log(bgimg.src);
-      
-      console.log(jsondata)
   });
 
   infobox.style.display = "block";
@@ -45,4 +68,12 @@ const userAction = (city) =>{
 
 checkbutton.addEventListener('click',()=>{
     userAction(cityinput.value);
+})
+
+morebtn.addEventListener('click',()=>{
+  if(details.style.display==="none" || details.style.display===""){
+    details.style.display="block";
+  }else if(details.style.display==="block"){
+    details.style.display="none";
+  }
 })
